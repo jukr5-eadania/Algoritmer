d53071156c86921340b2e65896fd7e3db3063768
jukr5-eadania <jukr5@student.eadania.dk>
2025-02-17 11:56:17 +0100

    Using Tests

---
+++ Algoritmer/Calculator/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("CalculatorTest")]
+
+namespace Calculator
+{
+    internal class Calculator
+    {
+        public float Add(float first, float second)
+        {
+            return first + second;
+        }
+    }
+}
---
+++ Algoritmer/CalculatorTest/CalculatorTest1.cs
@@ -0,0 +1,23 @@
+﻿namespace CalculatorTest
+{
+    [TestClass]
+    public sealed class CalculatorTest1
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            //Arrange
+            Calculator.Calculator calculator = new();
+            float first = 15;
+            float second = 10;
+            float expected = 25;
+
+            //Act
+            float actual = calculator.Add(first, second);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
