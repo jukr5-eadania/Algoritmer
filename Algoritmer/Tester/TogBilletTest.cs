ff12e4ff10bd6a97e9f26aaf57a6b5f86e8fa426
jukr5-eadania <jukr5@student.eadania.dk>
2025-02-17 12:47:02 +0100

    TogBillettest

---
+++ Algoritmer/Tester/TogBilletTest.cs
@@ -0,0 +1,64 @@
+﻿namespace Tester
+{
+    [TestClass]
+    public sealed class TogBilletTest
+    {
+        TogBillet.TogBillet togbillet = new();
+
+        [TestMethod]
+        public void TestBilletBaby()
+        {
+            //Arrange
+            int age = 0;
+            int expected = 0;
+
+            //Act
+            int actual = togbillet.BilletPris(age);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestBilletBørn()
+        {
+            //Arrange
+            int age = 14;
+            int expected = 10;
+
+            //Act
+            int actual = togbillet.BilletPris(age);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestBilletVoksen()
+        {
+            //Arrange
+            int age = 34;
+            int expected = 20;
+
+            //Act
+            int actual = togbillet.BilletPris(age);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestBilletPensioneret()
+        {
+            //Arrange
+            int age = 77;
+            int expected = 5;
+
+            //Act
+            int actual = togbillet.BilletPris(age);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Algoritmer/TogBillet/TogBillet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("Tester")]
+
+namespace TogBillet
+{
+    internal class TogBillet
+    {
+        public int BilletPris(int age)
+        {
+            switch (age)
+            {
+                case <= 1:
+                    return 0;
+
+                case >= 2 and <= 14:
+                    return 10;
+
+                case >= 15 and <= 64:
+                    return 20;
+
+                case >= 65:
+                    return 5;
+            }
+        }
+    }
+}
