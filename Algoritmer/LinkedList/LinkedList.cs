cb2e261a900fa1200aac8a9391d7345ba2185dce
jukr5-eadania <jukr5@student.eadania.dk>
2025-02-18 12:53:26 +0100

    Generic LinkedList

---
+++ Algoritmer/LinkedList/LinkedList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class LinkedList<T> : IEnumerable<T>
+    {
+        public Node<T> First { get; set; }
+
+        public Node<T> Last { get; set; }
+
+        public Node<T> AddFirst(T value)
+        {
+            Node<T> n = new Node<T>(value);
+
+            if (First == null)
+            {
+                First = n;
+                Last = First;
+            }
+            else
+            {
+                First.Previous = n;
+                n.Next = First;
+                First = n;
+            }
+            return n;
+        }
+
+        public Node<T> AddLast(T value)
+        {
+            Node<T> n = new Node<T>(value);
+
+            if (First == null)
+            {
+                First = n;
+                Last = First;
+            }
+            else
+            {
+                n.Previous = Last;
+                Last.Next = n;
+                Last = n;
+            }
+
+            return n;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = First;
+
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Algoritmer/LinkedList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            list.AddLast(23);
+            list.AddFirst(88);
+            list.AddFirst(1230);
+            list.AddLast(42);
+
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
