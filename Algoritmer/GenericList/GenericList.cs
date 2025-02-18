8c2ccf7521104f6df849f4f714958380c8a9a3bb
jukr5-eadania <jukr5@student.eadania.dk>
2025-02-18 13:27:55 +0100

    Genericlist

---
+++ Algoritmer/GenericList/GenericList.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    internal class GenericList<T> : IEnumerable<T>
+    {
+        private T[] elements;
+
+        private int index = 0;
+
+        public GenericList()
+        {
+            elements = new T[4];
+        }
+
+        public void Add(T element)
+        {
+            if (index == elements.Length/2)
+            {
+                T[] tmp = elements.ToArray();
+
+                elements = new T[elements.Length * 2];
+
+                for (int i = 0; i < tmp.Length; i++)
+                {
+                    elements[i] = tmp[i];
+                }
+            }
+
+            elements[index] = element;
+
+            index++;
+        }
+
+        public void Remove(T element)
+        {
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (elements[i].Equals(element))
+                {
+                    for (int y = 0; y < elements.Length - 1; y++)
+                    {
+                        elements[y] = elements[y + 1];
+                        elements[y + 1] = default(T);
+                    }
+
+                    index--;
+                    break;
+                }
+            }
+        }
+
+        public void Clear()
+        {
+            index = 0;
+            for (int i = 0; i < elements.Length; i++)
+            {
+                elements[i] = default(T);
+            }
+        }
+
+        public void Sort()
+        {
+            Array.Sort(elements, 0, index);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var indexo = 0;
+            while (indexo < index)
+            {
+                yield return elements[indexo];
+                indexo++;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Algoritmer/GenericList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+
+            list.Add(6);
+            list.Add(3);
+            list.Add(1);
+            list.Add(2);
+            list.Add(8);
+            list.Sort();
+            foreach (var item in list)
+            {
+                Console.WriteLine("eye: " + item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
