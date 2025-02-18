3c4df12664489fa2439306f8ce483d511385b32a
jukr5-eadania <jukr5@student.eadania.dk>
2025-02-18 13:06:33 +0100

    Highscore

---
+++ Algoritmer/HighScore/HighScore.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighScore
+{
+    internal class HighScore : IComparable<HighScore>
+    {
+        public string name;
+        public int score;
+
+        public HighScore(string name, int score)
+        {
+            this.name = name;
+            this.score = score;
+        }
+
+        public int CompareTo(HighScore other)
+        {
+            if (other.score > score)
+            {
+                return 1;
+            }
+            else if (other.score < score)
+            {
+                return -1;
+            }
+            else
+            {
+                return name.CompareTo(other.name);
+            }
+        }
+    }
+}
---
+++ Algoritmer/HighScore/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighScore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<HighScore> HighScore = new List<HighScore>();
+
+            HighScore.Add(new HighScore("Jane", 7));
+            HighScore.Add(new HighScore("John", 8));
+            HighScore.Add(new HighScore("Melanie", 9));
+            HighScore.Add(new HighScore("Kevin", 4));
+            HighScore.Add(new HighScore("Kenneth", 4));
+            HighScore.Add(new HighScore("Anders", 3));
+            HighScore.Add(new HighScore("Benny", 10));
+            HighScore.Add(new HighScore("Anders", 10));
+            HighScore.Add(new HighScore("Lars", 2));
+            HighScore.Add(new HighScore("Mette", 1));
+
+            HighScore.Sort();
+
+            foreach (var item in HighScore)
+            {
+                Console.Write(item.score + ": ");
+                Console.WriteLine(item.name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
