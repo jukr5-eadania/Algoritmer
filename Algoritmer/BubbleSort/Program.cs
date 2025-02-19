5f6b772679eb3b94a7e7380f6f798ee305d6e49b
jukr5-eadania <jukr5@student.eadania.dk>
2025-02-19 12:19:47 +0100

    BubbleSort

--- Algoritmer/BubbleSort/Program.cs
+++ Algoritmer/BubbleSort/Program.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InsertionSort
+namespace BubbleSort
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("InsertionSort:");
+            Console.WriteLine("BubbleSort:");
             int[] numberArray = new int[10] { 7, 332, 91, 2, 3, 73, 19, 96, 1, 75 };
             Console.WriteLine("The original Array of numbers is: ");
             foreach (var item in numberArray)
@@ -19,7 +19,7 @@ namespace InsertionSort
             }
             Console.WriteLine();
 
-            numberArray = InsertionSort(numberArray);
+            numberArray = BubbleSort(numberArray);
 
             Console.WriteLine("The sorted array of numbers is: ");
             foreach (var item in numberArray)
@@ -29,18 +29,23 @@ namespace InsertionSort
             Console.ReadKey();
         }
 
-        static int[] InsertionSort(int[] l)
+        static int[] BubbleSort(int[] l)
         {
-            for (int i = 1; i < l.Length; i++)
+            bool swapped = true;
+
+            while (swapped)
             {
-                int val = l[i];
-                int pointer = i;
-                while (pointer > 0 && val < l[pointer - 1])
+                swapped = false;
+                for (int i = 1; i < l.Length; i++)
                 {
-                    l[pointer] = l[pointer - 1];
-                    pointer = pointer - 1;
+                    if (l[i - 1] > l[i])
+                    {
+                        int tmp = l[i];
+                        l[i] = l[i - 1];
+                        l[i - 1] = tmp;
+                        swapped = true;
+                    }
                 }
-                l[pointer] = val;
             }
 
             return l;
