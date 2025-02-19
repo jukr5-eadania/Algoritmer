bdb24b11a80410e03fc718613e257c4b27efdb23
jukr5-eadania <jukr5@student.eadania.dk>
2025-02-19 10:18:25 +0100

    Beginner Algoritms

---
+++ Algoritmer/BeginnerAlgoritm/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BeginnerAlgoritm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 3, 6, 21, 73, 2, 882, 5, 83 };
+            Console.WriteLine("The original list is: ");
+            foreach (var item in list)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine();
+
+            int highestNumber = FindHighestNumber(list);
+            Console.WriteLine($"Highest number is: {highestNumber}");
+
+            List<int> oddNumberList = new List<int>();
+            oddNumberList = OddNumbers(list);
+            Console.WriteLine("The odd numbers are: ");
+            foreach (var item in oddNumberList)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.ReadLine();
+        }
+
+
+        static int FindHighestNumber(List<int> l)
+        {
+
+            int largest = l[0];
+            foreach (var item in l)
+            {
+                if (item > largest)
+                {
+                    largest = item;
+                }
+            }
+
+            return largest;
+        }
+
+        static List<int> OddNumbers(List<int> l)
+        {
+            List<int> oddNumberList = new List<int>();
+
+            foreach (int number in l)
+            {
+                if (number % 2 == 0)
+                {
+                    //is even
+                }
+                else
+                {
+                    //is odd
+                    oddNumberList.Add(number);
+                }
+            }
+
+            return oddNumberList;
+        }
+    }
+}
---
+++ Algoritmer/BeginnerAlgoritm/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BeginnerAlgoritm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BeginnerAlgoritm")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e0ad7202-e378-42ba-bc42-c0344bc55e1c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
