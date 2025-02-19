a25924c65448e6b05da9858a1b5a94ddb3ee0f1d
jukr5-eadania <jukr5@student.eadania.dk>
2025-02-19 10:57:30 +0100

    InsertionSort

---
+++ Algoritmer/InsertionSort/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numberArray = new int[10] { 7, 332, 91, 2, 3, 73, 19, 96, 1, 75 };
+            Console.WriteLine("The original Array of numbers is: ");
+            foreach (var item in numberArray)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine();
+
+            numberArray = InsertionSort(numberArray);
+
+            Console.WriteLine("The sorted array of numbers is: ");
+            foreach (var item in numberArray)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.ReadKey();
+        }
+
+        static int[] InsertionSort(int[] l)
+        {
+            for (int i = 1; i < l.Length - 1; i++)
+            {
+                int val = l[i];
+                int pointer = i;
+                while (pointer > 0 && val < l[pointer - 1])
+                {
+                    l[pointer] = l[pointer - 1];
+                    pointer = pointer - 1;
+                }
+                l[pointer] = val;
+            }
+
+            return l;
+        }
+    }
+}
--- Algoritmer/InsertionSort/Properties/AssemblyInfo.cs
+++ Algoritmer/InsertionSort/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("InsertionSort")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("InsertionSort")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1f219b3f-1bfe-4281-ad61-333bc2eb6040")]
+[assembly: Guid("515d4de1-ed82-45ff-8dcd-7cf9a385be04")]
 
 // Version information for an assembly consists of the following four values:
 //
