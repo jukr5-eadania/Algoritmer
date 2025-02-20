ddddb3048eb19c236938d4e69007a506c8d9cb97
jukr5-eadania <jukr5@student.eadania.dk>
2025-02-20 13:28:29 +0100

    BFS & DFS done

--- Algoritmer/Grafer/Program.cs
+++ Algoritmer/Grafer/Program.cs
@@ -38,7 +38,7 @@ namespace Grafer
 
             graph.AddEdge("FunHouse", "3D Cinema");
 
-            graph.AddEdge("3D Cinema", " Pirate Ship");
+            graph.AddEdge("3D Cinema", "Pirate Ship");
 
             graph.AddEdge("Slot Machines", "Hotdogs");
             graph.AddEdge("Slot Machines", "Rocket Ships");
@@ -54,9 +54,70 @@ namespace Grafer
             graph.AddEdge("Big Dipper", "Ghost Train");
 
             graph.AddEdge("Ghost Train", "Carousel");
-            graph.AddEdge("Ghost Train", " Flying Chairs");
+            graph.AddEdge("Ghost Train", "Flying Chairs");
 
             graph.AddEdge("Carousel", "Flying Chairs");
+
+            Node<string> n = DFS<string>(graph.Nodes.Find(x => x.Data == "Entrance"), graph.Nodes.Find(x => x.Data == "Ghost Train"));
+
+            List<Node<string>> path = TrackPath<string>(n, graph.Nodes.Find(x => x.Data == "Entrance"));
+
+            foreach (Node<string> pathNode in path)
+            {
+                Console.WriteLine(pathNode.Data);
+            }
+
+            Console.ReadLine();
+        }
+
+        public static Node<T> DFS<T>(Node<T> start, Node<T> goal)
+        {
+            Stack<Edge<T>> edges = new Stack<Edge<T>>();
+            edges.Push(new Edge<T>(start, start));
+
+            while (edges.Count > 0)
+            {
+                Edge<T> edge = edges.Pop();
+
+                if (!edge.To.Discovered)
+                {
+                    edge.To.Discovered = true;
+                    edge.To.Parent = edge.From;
+                }
+
+                if (edge.To == goal)
+                {
+                    return edge.To;
+                }
+
+
+                foreach (var e in edge.To.Edges)
+                {
+                    if (!e.To.Discovered)
+                    {
+                        edges.Push(e);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static List<Node<T>> TrackPath<T>(Node<T> node, Node<T> start)
+        {
+            List<Node<T>> path = new List<Node<T>>();
+
+            while (!node.Equals(start))
+            {
+                path.Add(node);
+                node = node.Parent;
+            }
+
+            path.Add(start);
+
+            path.Reverse();
+
+            return path;
         }
     }
 }
